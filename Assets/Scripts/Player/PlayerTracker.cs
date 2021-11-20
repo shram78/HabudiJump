4943919d9b7ece9666d33b809f92bc0567de9cc2
shram78 <ra00275@gmail.com>
2021-11-21 01:04:52 +0300

    folow camera comlete

--- Assets/Scripts/Player/PlayerTracker.cs
+++ Assets/Scripts/Player/PlayerTracker.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class PlayerTracker : MonoBehaviour
 {
     [SerializeField] private Player _player;
-    [SerializeField] private float _xOffset;
+    [SerializeField] private float _cameraSpeed;
+
+    private Vector3 _startPosition;
+
+    private void OnEnable()
+    {
+        _player.GameOver += ResetPosition;
+    }
+
+    private void OnDisable()
+    {
+        _player.GameOver -= ResetPosition;
+    }
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
 
     private void Update()
     {
-        transform.position = new Vector3(_player.transform.position.x - _xOffset, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x + (_cameraSpeed * Time.deltaTime), transform.position.y, transform.position.z);
+
+        if (_player.transform.localPosition.x >= transform.localPosition.x)
+            transform.position = new Vector3(_player.transform.localPosition.x, transform.position.y, transform.position.z);
+    }
+
+    private void ResetPosition()
+    {
+        transform.position = _startPosition;
     }
 }
