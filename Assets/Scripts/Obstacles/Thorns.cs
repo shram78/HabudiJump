a5f3856a6115a67a1899f3eed0ed7b9850293aea
shram78 <ra00275@gmail.com>
2021-11-28 21:14:11 +0300

    remake all obstacles

--- Assets/Scripts/Obstacles/Thorns.cs
+++ Assets/Scripts/Obstacles/Thorns.cs
@@ -3,17 +3,9 @@ using DG.Tweening;
 
 public class Thorns : MonoBehaviour
 {
-    private float _positionY = 0.1f;
-    private float _durationMove = 1f;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Player player))
             player.Die();
-    }
-
-    private void Start()
-    {
-        transform.DOLocalMoveY(_positionY, _durationMove).SetLoops(-1, LoopType.Yoyo);
     }
 }
